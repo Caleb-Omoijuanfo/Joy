28c25e262364f578d3a5ddd7881f491cae634ecd
Caleb-Omoijuanfo <c.omoijuanfo@gmail.com>
2022-07-04 10:40:34 +0100

    complete logic and other ui adjustments

--- src/components/Cards/ReasonCard/reasonCard.tsx
+++ src/components/Cards/ReasonCard/reasonCard.tsx
@@ -1,16 +1,51 @@
+import { useState } from "react";
+
+import Swal from "sweetalert2";
+import { data } from "../../../data";
+
 import "./reasonCard.css";
 
+interface Reason {
+  id: number;
+  content: string;
+}
+
 const ReasonCard = () => {
+  const [reasons] = useState<Reason[]>(data);
+  const [index, setIndex] = useState(0);
+
+  const handleClick = () => {
+    const totalNumberOfReasons = reasons.length - 1;
+
+    if (index < totalNumberOfReasons) {
+      setIndex((prevIndex) => prevIndex + 1);
+    } else {
+      Swal.fire({
+        title: "The End",
+        text: "I love you so much 😭. Start again?",
+        icon: "success",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setIndex(0);
+        }
+      });
+    }
+  };
+
   return (
     <div className="reason-card">
-      <h5 className="title">10 REASONS WHY I LOVE YOU #1</h5>
-      <p>
-        "It is easy to sit up and take notice, what's difficult is getting up
-        and taking action"
-      </p>
+      <h5 className="title">10 REASONS WHY I LOVE YOU #{reasons[index].id}</h5>
+      <div className="reason-container">
+        <p>"{reasons[index]?.content}"</p>
+      </div>
+
       <span className="divider" />
       <div className="random-button-container">
-        <span className="random-button" />
+        <span className="random-button" onClick={handleClick} />
       </div>
     </div>
   );
---
+++ src/data.ts
@@ -0,0 +1,48 @@
+export const data = [
+  {
+    id: 1,
+    content:
+      "For the first time in a long time, I am genuinely happy. You make me happy",
+  },
+  {
+    id: 2,
+    content:
+      "With you, I can be myself. I am absolutely comfortable around you",
+  },
+  {
+    id: 3,
+    content:
+      "You have the most beautiful face, smile and body. I could spend all day looking at you",
+  },
+  {
+    id: 4,
+    content: "You have my best Interest at heart. You talk to God about me",
+  },
+  {
+    id: 5,
+    content: "I love you because you chose me",
+  },
+  {
+    id: 6,
+    content:
+      "I love you because you are trying hard to become a better person for yourself and for your family",
+  },
+  {
+    id: 7,
+    content: "I cannot imagine my life without you",
+  },
+  {
+    id: 8,
+    content:
+      "You are my gist partner. We can talk about anything and everything",
+  },
+  {
+    id: 9,
+    content: "I love you because you are not afraid to be vulnerable with me",
+  },
+  {
+    id: 10,
+    content:
+      "You have the softest and most beautiful lips. I could kiss you all day",
+  },
+];
